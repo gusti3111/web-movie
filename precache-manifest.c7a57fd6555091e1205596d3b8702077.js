0ed44cd0aec98ec731347067fec11732a8c7c3ac
UKP-SEKRETARIAT <gusti.mispuansyah@gmail.com>
2020-06-17 18:26:41 +0700

    Updates

---
+++ precache-manifest.c7a57fd6555091e1205596d3b8702077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531f25e098f0e74bc95caf9a9bbb560f",
+    "url": "/web-movie/index.html"
+  },
+  {
+    "revision": "0136443fb477abc9be84",
+    "url": "/web-movie/static/css/main.a1d1d015.chunk.css"
+  },
+  {
+    "revision": "2700407db1b6546f37ea",
+    "url": "/web-movie/static/js/2.09c66b36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-movie/static/js/2.09c66b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0136443fb477abc9be84",
+    "url": "/web-movie/static/js/main.de034594.chunk.js"
+  },
+  {
+    "revision": "6a9d556b4f11e49509d6",
+    "url": "/web-movie/static/js/runtime-main.3ffb80aa.js"
+  }
+]);
